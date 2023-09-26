cd345b85aac4f303fc8fc41468ce56785c034dc3
laks-narasimman <124524141+laks-narasimman@users.noreply.github.com>
2023-09-26 22:39:37 +0530

    #32 added documentation on how to get back terraform if the tfstate file is lost or deleted. added documentation (#33)

--- providers.tf
+++ providers.tf
@@ -15,10 +15,10 @@ terraform {
  #   }
  # }
   required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
+    #random = {
+    #  source = "hashicorp/random"
+    #  version = "3.5.1"
+    #}
     aws = {
       source = "hashicorp/aws"
       version = "5.16.2"
@@ -28,6 +28,6 @@ terraform {
 
 provider "aws" {
 }
-provider "random" {
-  # Configuration options
-}
+#provider "random" {
+#  # Configuration options
+#}
