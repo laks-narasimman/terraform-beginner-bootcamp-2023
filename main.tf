c481b244f190000f70d5b3f2f71e4243b1cc0019
laks-narasimman <jainarasimma123@gmail.com>
2023-10-11 07:41:19 +0000

    #60 tested terratown in missingo and destroyed the same successfully, made several changes in the code

--- main.tf
+++ main.tf
@@ -23,30 +23,26 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567/api"
-  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
-  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint = var.terratowns_endpoint
+  user_uuid = var.teacherseat_user_uuid
+  token = var.terratowns_access_token
 }
 
-#module "terrahouse_aws" {
-#  source = "./modules/terrahouse_aws"
-#  user_uuid = var.user_uuid
-#  bucket_name = var.bucket_name
-#  index_html_filepath = var.index_html_filepath
-#  error_html_filepath = var.error_html_filepath
-#  content_version = var.content_version
-#}
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.teacherseat_user_uuid
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
+  content_version = var.content_version
+  assets_path = var.assets_path
+}
 
 resource "terratowns_home" "home" {
-  name = "How to play Arcanum in 2023!"
+  name = "Bahubali An Indian movie!"
   description = <<DESCRIPTION
-Arcanum is a game from 2001 that shipped with alot of bugs.
-Modders have removed all the originals making this game really fun
-to play (despite that old look graphics). This is my guide that will
-show you how to play arcanum without spoiling the plot.
+"Baahubali: The Beginning" is an epic Indian film that explores the rivalry between two royal brothers, Baahubali and Bhallaladeva, in the kingdom of Mahishmati. The movie culminates in a grand war between the brothers, filled with stunning visuals and breathtaking action sequences. This battle not only showcases their physical prowess but also tests their loyalty and morality. The emotional complexity of their conflict adds depth to the spectacle. "Baahubali: The Beginning" is a cinematic masterpiece, blending mythology, history, and fantasy, and it led to a highly successful sequel, solidifying its status as an iconic Indian film franchise.
 DESCRIPTION
-  #domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "3fdq3gz.cloudfront.net"
-  town = "gamers-grotto"
+  domain_name = module.terrahouse_aws.cloudfront_url
+  town = "missingo"
   content_version = 1
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,12 @@
-variable "user_uuid" {
+variable "terratowns_endpoint" {
+ type = string
+}
+
+variable "terratowns_access_token" {
+ type = string
+}
+
+variable "teacherseat_user_uuid" {
  type = string
 }
 
@@ -13,9 +21,11 @@ variable "index_html_filepath" {
 variable "error_html_filepath" {
   type = string
 }
+
 variable "content_version" {
   type        = number
 }
+
 variable "assets_path" {
   description = "Path to assets folder"
   type = string
